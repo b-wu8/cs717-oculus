b41fbe8fa238eefac637037e80a72c6f83902ead
Melody Hsu <mhsu13@jhu.edu>
2022-03-07 19:50:50 -0500

    updated comments and print statment

--- unity/BallController.cs
+++ unity/BallController.cs
@@ -1,15 +1,23 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Events;
+/*
+ * Event listener for packets from server machine.
+ * 
+ * Uses PrimaryButtonWatcher.cs to detect when a primary button is pressed 
+ * and rotates its parent GameObject. To use this class, add it to a visible 
+ * GameObject and drag the PrimaryButtonWatcher reference to the watcher 
+ * property in Unity.
+ */
 
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
-public class BallController : MonoBehaviour
-{
+public class BallController : MonoBehaviour {
+
     public PrimaryButtonWatcher watcher;
     public bool IsPressed = false; // used to display button state in the Unity Inspector window
     public Vector3 rotationAngle = new Vector3(45, 45, 45);
@@ -18,36 +26,43 @@ public class BallController : MonoBehaviour
     private Quaternion onRotation;
     private Coroutine rotator;
 
-    void Start()
-    {
+    /* 
+     * Start from Unity Engine.
+     */
+    void Start() {
+
         watcher.primaryButtonPress.AddListener(onPrimaryButtonEvent);
         offRotation = this.transform.rotation;
         onRotation = Quaternion.Euler(rotationAngle) * offRotation;
     }
 
+    /* 
+     * Sends back a message to server following an event.
+     */ 
     public void onPrimaryButtonEvent(bool pressed)
     {
         IsPressed = pressed;
-        if (rotator != null)
-            StopCoroutine(rotator);
-        if (pressed)
-        {
+        if (rotator != null) StopCoroutine(rotator);
+
+        if (pressed) {
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("128.220.221.21"), 4577);
             UdpClient client = new UdpClient();
             string text = "";
             byte[] data = Encoding.UTF8.GetBytes(text);
             client.Send(data, data.Length, remoteEndPoint);
             rotator = StartCoroutine(AnimateRotation(this.transform.rotation, onRotation));
-        }
-        else
+        } else {
             rotator = StartCoroutine(AnimateRotation(this.transform.rotation, offRotation));
+        }
     }
 
-    private IEnumerator AnimateRotation(Quaternion fromRotation, Quaternion toRotation)
-    {
+    /*
+     * Animate object change in position. 
+     */
+    private IEnumerator AnimateRotation(Quaternion fromRotation, Quaternion toRotation) {
+
         float t = 0;
-        while (t < rotationDuration)
-        {
+        while (t < rotationDuration) {
             transform.rotation = Quaternion.Lerp(fromRotation, toRotation, t / rotationDuration);
             t += Time.deltaTime;
             yield return null;
--- unity/UdpReceive.cs
+++ unity/UdpReceive.cs
@@ -1,29 +1,21 @@
 /*
- 
-    -----------------------
-    UDP-Receive (send to)
-    -----------------------
-    // [url]http://msdn.microsoft.com/de-de/library/bb979228.aspx#ID0E3BAC[/url]
-   
-   
-    // > receive
-    // 127.0.0.1 : 8051
-   
-    // send
-    // nc -u 127.0.0.1 8051
- 
-*/
-using UnityEngine;
-using System.Collections;
+ * Respond to packets from server machine.
+ */
 
 using System;
+using System.Collections;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Runtime.InteropServices;
 using System.Globalization;
+using UnityEngine;
 
+/*
+ * Packet sent to DSN Rain machine.
+ */
+ /*
 [StructLayout(LayoutKind.Sequential, Pack=1)]
 public unsafe struct RainPacket {
     public int flag;
@@ -32,49 +24,53 @@ public unsafe struct RainPacket {
     public double z;
     public fixed byte mess[256];
 };
+*/
 
-public class UdpReceive : MonoBehaviour
-{
+public class UdpReceive : MonoBehaviour {
+
     Thread receiveThread;
     UdpClient client;
     GameObject sphere;
-    public int port; // define > init
+    public int port; // defined in init()
     public string lastReceivedUDPPacket = "";
-    public string allReceivedUDPPackets = ""; // clean up this from time to time!
+    public string allReceivedUDPPackets = ""; // clean frequently
     public float x, y, z;
 
-    // start from shell
-    private static void Main()
-    {
+    /*
+     * Start from shell.
+     */
+    private static void Main() {
+
         UdpReceive receiveObj = new UdpReceive();
         receiveObj.init();
 
         string text = "";
-        do
-        {
+        do {
             text = Console.ReadLine();
-        }
-        while (!text.Equals("exit"));
+        } while (!text.Equals("exit"));
     }
     
     /*
      * Start from Unity Engine.
      */
-    public void Start()
-    {
+    public void Start() {
+
         init();
     }
 
     /*
      * Update object position at every time tick.
      */
-    public void Update()
-    {
+    public void Update() {
+
         sphere.transform.position = new Vector3(x, y, z);
     }
 
-    void OnGUI()
-    {
+    /*
+     * GUI for Unity.
+     */
+    void OnGUI() {
+
         Rect rectObj = new Rect(40, 10, 200, 400);
         GUIStyle style = new GUIStyle();
         style.alignment = TextAnchor.UpperLeft;
@@ -82,17 +78,18 @@ public class UdpReceive : MonoBehaviour
                     + "shell> nc -u 127.0.0.1 : " + port + " \n"
                     + "\nLast Packet: \n" + lastReceivedUDPPacket
                     + "\n\nAll Messages: \n" + allReceivedUDPPackets
-                , style);
+                    , style);
     }
 
-    // init
-    private void init()
-    {
+    /*
+     * Initialize virtual environment and thread.
+     */
+    private void init() {
+
         Debug.Log("UDPSend.init()");
+        port = 8051; // change port here 
 
-        port = 8051;
-
-        // status
+        // Status output
         Debug.Log("Sending to 127.0.0.1 : " + port);
         Debug.Log("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
 
@@ -112,20 +109,17 @@ public class UdpReceive : MonoBehaviour
      * Receive data from background thread.
      * Updates class variables that are used when Update() is called.  
      */
-    private void ReceiveData()
-    {
+    private void ReceiveData() {
 
         client = new UdpClient(port); // port to listen on
-
-        byte[] temp = Encoding.UTF8.GetBytes("Starting...");
-        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("128.220.221.21"), 4577);
+        byte[] temp = Encoding.UTF8.GetBytes("OCULUS");
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("128.220.221.21"), 4578);
         client.Send(temp, temp.Length, remoteEndPoint); // first packet to override weird 0.0.0.0 IP address packet 
         client.Send(temp, temp.Length, remoteEndPoint);
 
-        while (true)
-        {
-            try
-            {
+        // continuously accept server data
+        while (true) {
+            try {
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
                 byte[] data = client.Receive(ref anyIP); // blocks until data is received from somewhere 
                 string text = Encoding.UTF8.GetString(data); // convert struct to string
@@ -134,13 +128,11 @@ public class UdpReceive : MonoBehaviour
                 lastReceivedUDPPacket = text;
                 allReceivedUDPPackets = allReceivedUDPPackets + text;
 
-                /* Update object position asynchronously via Update thread*/
+                // Update object position asynchronously via Update thread 
                 x = float.Parse(pieces[0], CultureInfo.InvariantCulture.NumberFormat);
                 y = float.Parse(pieces[1], CultureInfo.InvariantCulture.NumberFormat);
                 z = float.Parse(pieces[2], CultureInfo.InvariantCulture.NumberFormat);
-            }
-            catch (Exception err)
-            {
+            } catch (Exception err) {
                 Debug.Log(err.ToString());
             }
         }
@@ -150,8 +142,8 @@ public class UdpReceive : MonoBehaviour
      * Get the most recent UDP packet.
      * clears previously received packets.
      */ 
-    public string getLatestUDPPacket()
-    {
+    public string getLatestUDPPacket() {
+
         allReceivedUDPPackets = "";
         return lastReceivedUDPPacket;
     }
