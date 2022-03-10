1ef01813d511de79b493bf69fd53aa33b26e8d22
JAY <jlei10@jhu.edu>
2022-03-09 20:50:21 -0500

    Update UdpReceive.cs

--- unity/UdpReceive.cs
+++ unity/UdpReceive.cs
@@ -15,7 +15,6 @@
 */
 using UnityEngine;
 using System.Collections;
-
 using System;
 using System.Text;
 using System.Net;
@@ -24,6 +23,7 @@ using System.Threading;
 using System.Runtime.InteropServices;
 using System.Globalization;
 
+/*
 [StructLayout(LayoutKind.Sequential, Pack=1)]
 public unsafe struct RainPacket {
     public int flag;
@@ -32,6 +32,48 @@ public unsafe struct RainPacket {
     public double z;
     public fixed byte mess[256];
 };
+*/
+
+public class PlayerInfo 
+{
+    public string name;
+    public Headset headset;
+    public LeftHandController left_hand;
+    public RightHandController right_hand;
+    public string timestamp;
+    
+    //string constructor
+    public PlayerInfo(string name, Headset headset, LeftHandController left_hand, RightHandController right_hand)
+    {
+        this.name = name;
+        this.headset = Headset.deep_copy(headset);
+        this.left_hand = LeftHandController.deep_copy(left_hand);
+        this.right_hand = RightHandController.deep_copy(right_hand);
+    }
+
+    vector3 StringToVec3(string str_x, string str_y, string str_z){
+        return new vector3(float.Parse(str_x), float.Parse(str_y), float.Parse(str_z));
+    }
+
+    vector2 StringToVec2(string str_x, string str_y){
+        return new vector2(float.Parse(str_x), float.Parse(str_y));
+    }
+
+    Quaternion StringToQuat(string str_x, string str_y, string str_z, string str_w){
+        return new Quaternion(float.Parse(str_x), float.Parse(str_y), float.Parse(str_z), float.Parse(str_w));
+    }
+
+    public PlayerInfo(string data)
+    {
+        string[] infos = data.Split(' ');
+        this.name = infos[0];
+        this.headset = new headset(StringToVec3(infos[1], infos[2], infos[3]), StringToQuat(info[4], info[5], info[6], info[7]));
+        this.left_hand = new LeftHandController(StringToVec3(infos[8], infos[9], infos[10]), StringToQuat(info[11], info[12], info[13], info[14]));
+        this.right_hand = new RightHandController(StringToVec3(infos[15], infos[16], infos[17]), StringToQuat(info[18], info[19], info[20], info[21]));
+        this.timestamp = infos[22];
+        
+    }
+}
 
 public class UdpReceive : MonoBehaviour
 {
@@ -41,7 +83,8 @@ public class UdpReceive : MonoBehaviour
     public Config config;
     public string lastReceivedUDPPacket = "";
     public string allReceivedUDPPackets = ""; // clean up this from time to time!
-    public float x, y, z;
+    // public float x, y, z;
+    List<PlayerInfo> all_player_info = new List<PlayerInfo>();
 
     // start from shell
     private static void Main()
@@ -115,7 +158,7 @@ public class UdpReceive : MonoBehaviour
 
         client = new UdpClient(config.local_port); // port to listen on
 
-        byte[] temp = Encoding.UTF8.GetBytes("OCULUS " + config.player_name + " " + config.lobby);
+        byte[] temp = Encoding.UTF8.GetBytes("OCULUS melody test");
         IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(config.remote_ip_address), config.remote_port);
         client.Send(temp, temp.Length, remoteEndPoint); // first packet to override weird 0.0.0.0 IP address packet 
         client.Send(temp, temp.Length, remoteEndPoint);
@@ -127,16 +170,35 @@ public class UdpReceive : MonoBehaviour
                 IPEndPoint remoteIP = new IPEndPoint(IPAddress.Parse(config.remote_ip_address), config.remote_port);
                 byte[] data = client.Receive(ref remoteIP); // blocks until data is received from somewhere 
                 string text = Encoding.UTF8.GetString(data); // convert struct to string
-                Debug.Log("Received data: " + text);
-                string[] pieces = text.Split(' '); // TODO: better encoding
+
+                string[] lines = text.Split("/n"); // TODO: better encoding
+                // 0 is lobby and # players
+                // 1 is object
+                // 2 is plane
+                for (int i = 3; i < lines.Length; i++) 
+                {
+                    string[] players = text[i].Split(' ');
+                    if (all_player_info.Exists(x => x.name.Equals(players[0])))
+                    {
+                        continue; // player already added
+                    } 
+                    else
+                    {
+                        PlayerInfo new_player = new PlayerInfo(players[0], players[1], players[2], players[3]);
+                    }
+                } 
+
                 Debug.Log(">> " + text);
                 lastReceivedUDPPacket = text;
                 allReceivedUDPPackets = allReceivedUDPPackets + text;
+
                 /* Update object position asynchronously via Update thread*/
+                /*
                 x = float.Parse(pieces[0], CultureInfo.InvariantCulture.NumberFormat);
                 y = float.Parse(pieces[1], CultureInfo.InvariantCulture.NumberFormat);
                 z = float.Parse(pieces[2], CultureInfo.InvariantCulture.NumberFormat);
                 Debug.Log("pos rcvd:" + "X("+x+") Y("+y+") Z("+z+")");
+                */
             }
             catch (Exception err)
             {
@@ -144,6 +206,7 @@ public class UdpReceive : MonoBehaviour
             }
         }
     }
+
 
     /*
      * Get the most recent UDP packet.
